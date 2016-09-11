8890adc1f0bcb98990bf1a36b563182cccb0983b
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-09-11 15:59:53 +0000

    Mass updates

--- js/app.js
+++ js/app.js
@@ -1,15 +1,46 @@
 var screenWidth = document.documentElement.clientWidth;
 var screenHeight = document.documentElement.clientHeight;
 
-var xGapStart = 25;
-var xGapEnd = 175;
-var yGapStart = 25;
-var yGapEnd = 150;
+var xGapStart = 75;
+var xGapEnd = 75;
+var yGapStart = 75;
+var yGapEnd = 75;
 
-function calculatePosition (width, height, xGapStart, xGapEnd, yGapStart, yGapEnd, teamNumber, numberOfTeams) {
+var teamSpriteWidth = 64;
+var teamSpriteHeight = 64;
+
+var fireballSpriteWidth = 128;
+var fireballSpriteHeight = 128;
+
+var fontSize = 18;
+var labelYGap = 5;
+
+function getUpColor(serviceId) {
+    switch (serviceId) {
+        case 1: return '#f00';
+        case 2: return '#ff0';
+        case 3: return '#00f';
+        case 4: return '#0f0';
+        case 5: return '#fff';
+        default: return '#fff';
+    }
+}
+
+function getDownColor(serviceId) {
+    switch (serviceId) {
+        case 1: return '#400';
+        case 2: return '#440';
+        case 3: return '#004';
+        case 4: return '#040';
+        case 5: return '#444';
+        default: return '#444';
+    }
+}
+
+function calculatePosition (teamNumber, numberOfTeams) {
     var t = 2 * Math.PI * teamNumber / numberOfTeams
-    var a = (width - xGapStart - xGapEnd) / 2
-    var b = (height - yGapStart - yGapEnd) / 2
+    var a = (screenWidth - xGapStart - xGapEnd) / 2
+    var b = (screenHeight - yGapStart - yGapEnd) / 2
     var x = a * Math.cos(t)
     var y = b * Math.sin(t)
     return {
@@ -18,69 +49,39 @@ function calculatePosition (width, height, xGapStart, xGapEnd, yGapStart, yGapEn
     }
 }
 
-function getServices () {
-    return [
-        {
-            serviceId: 1,
-            serviceState: 2,
-            sprite: null
-        },
-        {
-            serviceId: 2,
-            serviceState: 2,
-            sprite: null
-        },
-        {
-            serviceId: 3,
-            serviceState: 2,
-            sprite: null
-        },
-        {
-            serviceId: 4,
-            serviceState: 2,
-            sprite: null
-        }
-    ]
+function getQuadrant (teamNumber, numberOfTeams) {
+    var angle = 2 * Math.PI * teamNumber / numberOfTeams;
+    if (angle >= 0.0 && angle < (Math.PI / 2)) {
+        return 'topRight';
+    } else if (angle >= Math.PI / 2 && angle < Math.PI) {
+        return 'topLeft';
+    } else if (angle > Math.PI && angle < (3 * Math.PI / 2)) {
+        return 'bottomLeft';
+    } else if (angle >= (3 * Math.PI / 2) && angle < (2 * Math.PI)) {
+        return 'bottomRight';
+    } else {
+        return 'unknown';
+    }
 }
 
 var app = {
     fireActionsCache: [],
     options: {
-        teams: [
-            {teamId: 1, services: getServices(), name: 'FTeam', position: calculatePosition(screenWidth, screenHeight, xGapStart, xGapEnd, yGapStart, yGapEnd, 0, 9), labelPosition: {x: 20, y: 10} },
-            {teamId: 2, services: getServices(), name: 'EpicFairPlay', position: calculatePosition(screenWidth, screenHeight, xGapStart, xGapEnd, yGapStart, yGapEnd, 1, 9), labelPosition: {x: -20, y: 10} },
-            {teamId: 3, services: getServices(), name: 'Koibasta', position: calculatePosition(screenWidth, screenHeight, xGapStart, xGapEnd, yGapStart, yGapEnd, 2, 9), labelPosition: {x: 10, y: 10} },
-            {teamId: 4, services: getServices(), name: 'KED', position: calculatePosition(screenWidth, screenHeight, xGapStart, xGapEnd, yGapStart, yGapEnd, 3, 9), labelPosition: {x: 40, y: 10} },
-            {teamId: 5, services: getServices(), name: 'Пиксели', position: calculatePosition(screenWidth, screenHeight, xGapStart, xGapEnd, yGapStart, yGapEnd, 4, 9), labelPosition: {x: 0, y: 10} },
-            {teamId: 6, services: getServices(), name: 'Bushwhackers', position: calculatePosition(screenWidth, screenHeight, xGapStart, xGapEnd, yGapStart, yGapEnd, 5, 9), labelPosition: {x: -40, y: 10} },
-            {teamId: 7, services: getServices(), name: 'UFOlogists', position: calculatePosition(screenWidth, screenHeight, xGapStart, xGapEnd, yGapStart, yGapEnd, 6, 9), labelPosition: {x: 0, y: 10} },
-            {teamId: 8, services: getServices(), name: 'PWNCult', position: calculatePosition(screenWidth, screenHeight, xGapStart, xGapEnd, yGapStart, yGapEnd, 7, 9), labelPosition: {x: 10, y: 10} },
-            {teamId: 9, services: getServices(), name: 'SiBears', position: calculatePosition(screenWidth, screenHeight, xGapStart, xGapEnd, yGapStart, yGapEnd, 8, 9), labelPosition: {x: 20, y: 10} }
-        ],
-        services: [
-            {serviceId: 1, serviceName: 'digidocs', upColor: '#f00', downColor: '#400' },
-            {serviceId: 2, serviceName: 'mobile-profile', upColor: '#ff0', downColor: '#440' },
-            {serviceId: 3, serviceName: 'sociality', upColor: '#00f', downColor: '#004' },
-            {serviceId: 4, serviceName: 'weather', upColor: '#0f0', downColor: '#040' }
-        ],
         width: screenWidth,
         height: screenHeight
     },
 
-    //initServices: function(){}
-
-    init: function(){
-
+    init: function() {
         var self = this;
-
         var game = new Phaser.Game(self.options.width, self.options.height, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render });
 
         function preload() {
-            game.load.spritesheet('ball', 'assets/particles/plasmaball.png', 128, 128);
-            game.load.spritesheet('red', 'assets/particles/red.png', 128, 128);
-            game.load.spritesheet('yellow', 'assets/particles/yellow.png', 128, 128);
-            game.load.spritesheet('blue', 'assets/particles/blue.png', 128, 128);
-            game.load.spritesheet('green', 'assets/particles/green.png', 128, 128);
+            game.load.spritesheet('bubble', 'assets/particles/bubble.png', teamSpriteWidth, teamSpriteHeight);
+            game.load.spritesheet('red', 'assets/particles/red.png', fireballSpriteWidth, fireballSpriteHeight);
+            game.load.spritesheet('yellow', 'assets/particles/yellow.png', fireballSpriteWidth, fireballSpriteHeight);
+            game.load.spritesheet('blue', 'assets/particles/blue.png', fireballSpriteWidth, fireballSpriteHeight);
+            game.load.spritesheet('green', 'assets/particles/green.png', fireballSpriteWidth, fireballSpriteHeight);
+            game.load.spritesheet('white', 'assets/particles/white.png', fireballSpriteWidth, fireballSpriteHeight);
         }
 
         var sprite;
@@ -126,20 +127,32 @@ var app = {
 
             sprite.filters = [ filter ];
 
-            //create players
-            self.options.teams.forEach(function(team){
+            // var logo = game.add.sprite(screenWidth / 2 - 256, screenHeight / 2 - 256, 'volgactf');
 
-                var player = game.add.sprite(team.position.x, team.position.y, 'ball');
+            self.options.teams.forEach(function(team, ndx, arr) {
+                team.position = calculatePosition(ndx, arr.length);
+                var quadrant = getQuadrant(ndx, arr.length);
 
-                initServices(team);
+                var player = game.add.sprite(team.position.x - teamSpriteWidth / 2, team.position.y - teamSpriteHeight / 2, 'bubble');
 
-                //filterL = game.add.filter('LightBeam', 800, 600);
-                //player.filters = [filterL];
+                initServices(team, quadrant);
 
-                createText(team.position.x + team.labelPosition.x, team.position.y - team.labelPosition.y, team.name);
+                var teamLabelX = team.position.x - teamSpriteWidth / 2;
+                var teamLabelY;
+                switch (quadrant) {
+                    case 'bottomLeft':
+                    case 'bottomRight':
+                        teamLabelY = team.position.y - teamSpriteHeight / 2 - fontSize - labelYGap - 5;
+                        break;
+                    case 'topLeft':
+                    case 'topRight':
+                    default:
+                        teamLabelY = team.position.y + teamSpriteHeight / 2 + labelYGap + 5;
+                        break;
+                }
+
+                createText(teamLabelX, teamLabelY, team.name);
             });
-            ////////////////
-            //onAction DEMO - TEST
 
             fetch('/api/team/services')
             .then((response) => {
@@ -167,12 +180,9 @@ var app = {
                 })
             })
 
-
+            // Test
             // setTimeout(function() {
             //     onAttack(1, 4, 1);
-            //     // setTimeout(function(){
-            //     //     onServiceStateChange({teamId: 2, serviceId: 3, state: 'down'});
-            //     // }, 6000);
             // }, 3000);
 
             // setTimeout(function() {
@@ -182,11 +192,35 @@ var app = {
             // setTimeout(function() {
             //     onAttack(3, 6, 3);
             // }, 4000);
-            // /////////////////////
 
             // setTimeout(function() {
             //     onAttack(4, 7, 4);
             // }, 4500);
+
+            // setTimeout(function() {
+            //     onAttack(3, 8, 5);
+            // }, 5500);
+
+            // setTimeout(function() {
+            //     onAttack(5, 12, 4);
+            // }, 6000);
+
+            // setTimeout(function() {
+            //     onAttack(7, 14, 5);
+            // }, 6500);
+
+            // setTimeout(function() {
+            //     onAttack(8, 1, 1);
+            // }, 7000);
+
+            // setTimeout(function() {
+            //     onAttack(10, 2, 3);
+            // }, 7500);
+
+            // setTimeout(function() {
+            //     onAttack(13, 6, 4);
+            // }, 8000);
+
 
             // setTimeout(function() {
             //     onServiceStateChange(1, 1, 'up');
@@ -201,21 +235,36 @@ var app = {
             //     onServiceStateChange(5, 3, 'up');
             //     onServiceStateChange(5, 4, 'up');
             // }, 4000)
+            // end test
         }
 
-        function initServices(team) {
-            //console.log(game);
-            team.services.forEach(function(record) {
+        function initServices(team, quadrant) {
+            var totalHeight = teamSpriteHeight + labelYGap * 2;
+            var gapHeight = 5;
+            var totalGapHeight = gapHeight * (team.services.length - 1);
+            var itemHeight = (totalHeight - totalGapHeight) / team.services.length;
+            var itemWidth = itemHeight;
+            var marginX = 10;
+
+            team.services.forEach(function(record, ndx) {
                 var service = self.options.services.find(function(service) {
                     return service.serviceId === record.serviceId;
                 });
-                if (!record.sprite) {
-                    record.sprite = new Phaser.Rectangle(team.position.x + 130, team.position.y + 25 + (record.serviceId - 1) * 20, 15, 15);
+                var itemPositionX;
+                switch (quadrant) {
+                    case 'topLeft':
+                    case 'bottomLeft':
+                        itemPositionX = team.position.x - teamSpriteWidth / 2 - marginX - itemWidth;
+                        break;
+                    case 'topRight':
+                    case 'bottomRight':
+                    default:
+                        itemPositionX = team.position.x + teamSpriteWidth / 2 + marginX;
+                        break;
                 }
-                game.debug.geom(record.sprite, (record.serviceState === 1) ? service.upColor : service.downColor);
-                //game.debug.renderRectangle(serviceObj.sprite,'#0fffff');
-                //console.log(serviceObj.sprite);
-
+                var itemPositionY = team.position.y - teamSpriteHeight / 2 - labelYGap + ndx * (itemHeight + gapHeight);
+                record.sprite = new Phaser.Rectangle(itemPositionX, itemPositionY, itemWidth, itemHeight);
+                game.debug.geom(record.sprite, (record.serviceState === 1) ? getUpColor(service.serviceId) : getDownColor(service.serviceId));
             });
         }
 
@@ -235,7 +284,6 @@ var app = {
             });
 
             filter.update(game.input.activePointer);
-
         }
 
         function onAttack (attackerId, victimId, serviceId) {
@@ -247,31 +295,39 @@ var app = {
                 return team.teamId === victimId;
             });
 
-            //console.log(targetObj.services[serviceId].sprite);
-            //fix fixed service id
-            var sPos = sourceObj.position;
-            var tPos = targetObj.position;
+            var sourcePos = {
+                x: sourceObj.position.x - fireballSpriteWidth / 2,
+                y: sourceObj.position.y - fireballSpriteHeight / 2
+            };
+            var targetPos = {
+                x: targetObj.position.x - fireballSpriteWidth / 2,
+                y: targetObj.position.y - fireballSpriteHeight / 2
+            };
 
-            var spriteName = 'ball'
+            var spriteName = 'bubble';
             switch (serviceId) {
                 case 1: spriteName = 'red'; break;
                 case 2: spriteName = 'yellow'; break;
                 case 3: spriteName = 'blue'; break;
                 case 4: spriteName = 'green'; break;
+                case 5: spriteName = 'white'; break;
                 default: break
             }
 
             var fireAct = {
-                sprite: game.add.sprite(sPos.x, sPos.y, spriteName),
-                targetPosition: {x: tPos.x, y: tPos.y},
-                posVariable: {x: (tPos.x - sPos.x) / 120,
-                              y: (tPos.y - sPos.y) / 120}
+                sprite: game.add.sprite(sourcePos.x, sourcePos.y, spriteName),
+                targetPosition: {
+                    x: targetPos.x,
+                    y: targetPos.y
+                },
+                posVariable: {
+                    x: (targetPos.x - sourcePos.x) / 120,
+                    y: (targetPos.y - sourcePos.y) / 120
+                }
             };
             self.fireActionsCache.push(fireAct);
         }
-        ////////////
 
-        // Смена статуса сервиса
         function onServiceStateChange(teamId, serviceId, serviceState){
             var team = self.options.teams.find(function(team) {
                 return team.teamId === teamId;
@@ -283,40 +339,80 @@ var app = {
 
             record.serviceState = serviceState
 
-            self.options.teams.forEach(function(team) {
-                initServices(team)
+            self.options.teams.forEach(function(team, ndx, arr) {
+                var quadrant = getQuadrant(ndx, arr.length);
+                initServices(team, quadrant);
             })
         }
-        ///////////////
 
-        function createText(x, y, string) {
-
-            var text = game.add.text(x, y, string);
+        function createText(x, y, textStr) {
+            var text = game.add.text(x, y, textStr);
             // text.anchor.set(0.5);
             // text.align = 'center';
 
             //  Font style
             text.font = 'Arial Black';
-            text.fontSize = 20;
+            text.fontSize = fontSize;
             // text.fontWeight = 'bold';
             text.fill = '#ffffff';
             //text.setShadow(2, 2, 'rgba(0, 0, 0, 0.7)', 2);
 
             return text;
-
         }
 
-
         function render() {
-
-            // game.debug.bodyInfo(sprite, 32, 32);
-
         }
     }
 }
 
-window.onload = function(){
+window.onload = function() {
+    fetch('/api/services')
+    .then(function(response) {
+        if (response.status >= 200 && response.status < 300) {
+            return response.json()
+        } else {
+            let err = new Error(response.statusText)
+            err.response = response
+            throw err
+        }
+    })
+    .then(function(servicesData) {
+        var servicesOptions = servicesData.map(function(serviceData) {
+            return {
+                serviceId: serviceData.id,
+                serviceName: serviceData.name
+            }
+        });
 
-    app.init();
+        fetch('/api/teams')
+        .then(function(response) {
+            if (response.status >= 200 && response.status < 300) {
+                return response.json()
+            } else {
+                let err = new Error(response.statusText)
+                err.response = response
+                throw err
+            }
+        })
+        .then(function(teamsData) {
+            var teamsOptions = teamsData.map(function(teamData) {
+                return {
+                    teamId: teamData.id,
+                    services: servicesData.map(function(serviceData) {
+                        return {
+                            serviceId: serviceData.id,
+                            serviceState: 2,
+                            sprite: null
+                        };
+                    }),
+                    name: teamData.name
+                }
+            });
+
+            app.options.services = servicesOptions;
+            app.options.teams = teamsOptions;
+            app.init();
+        });
+    });
 
 }
